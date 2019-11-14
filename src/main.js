bb551169f8c2e4a98b5458bc0ea97b9a6adb374d
Steven Turner <steven.turner12x@gmail.com>
2019-11-13 22:40:27 -0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,87 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      items: [
+        {
+          title: "HTML5",
+          edit: false
+        },
+        {
+          title: "CSS3",
+          edit: false
+        },
+        {
+          title: "Javascript",
+          edit: false
+        },
+        {
+          title: "Java",
+          edit: false
+        }
+      ],
+      taskItem: "",
+      isError: false
+    };
+  },
+  methods: {
+    addItem: function (e) {
+      const value = e.target.value;
+
+      if (value === "") {
+        return false;
+      }
+      if (this.itemExist(value) === true) {
+        let repeated = [].filter.call(document.getElementsByTagName("span"),
+            function (span) {
+              return span.textContent === value;
+            }
+        );
+        this.isError = true;
+        repeated[0].classList.add("bg-orange-300");
+        return false;
+      }
+      this.isError = false;
+      e.target.value = "";
+      this.items.push({
+        title: value,
+        edit: false
+      });
+
+      let repeatedClasses = document.querySelectorAll(".bg-orange-300");
+      for (let repeatedClass of repeatedClasses) {
+        repeatedClass.classList.remove("bg-orange-300");
+      }
+    },
+    removeItem: function (index) {
+      this.items.splice(index, 1);
+      this.$refs.addtask.focus();
+    },
+    enableEdit: function (index) {
+      this.items[index].edit = true;
+      this.$nextTick(() => {
+        let input = this.$refs.task[0];
+        input.focus()
+      });
+    },
+    disableEdit: function (index) {
+      this.item[index].edit = false;
+    },
+    itemExist: function (value) {
+      for (var i = 0; i < this.items.length; i++) {
+        if (this.items[i].title === value) {
+          return true;
+        }
+      }
+      return false;
+    }
+  },
+
+
+  render: h => h(App),
+}).$mount('#app')
